113eb86fae299f15a0e4e1680ca8eeb749f956b5
Giorgio Rindonone <giorgio.rindonone@gmail.com>
2021-07-09 17:15:58 +0200

    fix colors 2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -55,62 +55,66 @@
 </footer> --}}
 
 <footer class="site-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12 col-md-6">
-          <h6>About</h6>
-          <p class="text-justify">Booleat.com <i> Ordina semplicemente dal tuo divano! </i> Potrai scegliere tra diverse categorie per ogni pietanza, la nostra selezione di ristoranti offre diverse varietà di cibi da ordinare, ti basterà scegliere il tuo ristorante preferito e con un paio di click ti arriverà tutto comodamente a casa!! </p>
-        </div>
-
-     
-
-        <div class="col-xs-6 col-md-3">
-          <h6>About Us</h6>
-          <ul class="footer-links">
-            <li><span>Alessandro Marega</span> 
-                <span>
-                    <a href="https://github.com/maregaale"><i class="fab fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/alessandro-marega-58b767213/"><i class="fab fa-linkedin"></i></a>
-                </span>
-            </li>
-            <li><span>Daniele Lai </span> 
-                <span>
-                    <a href="https://github.com/daniele-lai"><i class="fab fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/daniele-lai-b01577214/"><i class="fab fa-linkedin"></i></a>
-                </span>
-            </li>
-            <li><span>Gianluca Forgia </span>
-                <span>
-                    <a href="https://github.com/gianluca-forgia95"><i class="fab fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/gianluca-forgia-a94548212/"><i class="fab fa-linkedin"></i></a>
-                </span>
-            </li>
-            <li><span>Giorgio Rindonone</span> 
-                <span>
-                    <a href="https://github.com/GiorgioRindonone"><i class="fab fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/giorgio-rindonone-516b961b0/"><i class="fab fa-linkedin"></i></a>
-                </span>
-            </li>
-            <li><span>Gianluigi Spisto </span>
-                <span>
-                    <a href="https://github.com/glSpisto-92"><i class="fab fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/gianluigi-spisto-8ab951190/"><i class="fab fa-linkedin"></i></a>
-                </span>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <hr>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-sm-6 col-xs-12">
-          <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
-       <a href="#">Booleat</a>.
-          </p>
-        </div>
-
+    <div  class="site-footer-cont">
+        <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p class="text-justify">Booleat.com <i> Ordina semplicemente dal tuo divano! </i> Potrai scegliere tra diverse categorie per ogni pietanza, la nostra selezione di ristoranti offre diverse varietà di cibi da ordinare, ti basterà scegliere il tuo ristorante preferito e con un paio di click ti arriverà tutto comodamente a casa!! </p>
+              </div>
       
-      </div>
+           
+      
+              <div class="col-sm-12 col-md-6">
+                <h6>About Us</h6>
+                <ul class="footer-links">
+                  <li><span>Alessandro Marega</span> 
+                      <span>
+                          <a href="https://github.com/maregaale"><i class="fab fa-github"></i></a> 
+                          <a href="https://www.linkedin.com/in/alessandro-marega-58b767213/"><i class="fab fa-linkedin"></i></a>
+                      </span>
+                  </li>
+                  <li><span>Daniele Lai </span> 
+                      <span>
+                          <a href="https://github.com/daniele-lai"><i class="fab fa-github"></i></a> 
+                          <a href="https://www.linkedin.com/in/daniele-lai-b01577214/"><i class="fab fa-linkedin"></i></a>
+                      </span>
+                  </li>
+                  <li><span>Gianluca Forgia </span>
+                      <span>
+                          <a href="https://github.com/gianluca-forgia95"><i class="fab fa-github"></i></a> 
+                          <a href="https://www.linkedin.com/in/gianluca-forgia-a94548212/"><i class="fab fa-linkedin"></i></a>
+                      </span>
+                  </li>
+                  <li><span>Giorgio Rindonone</span> 
+                      <span>
+                          <a href="https://github.com/GiorgioRindonone"><i class="fab fa-github"></i></a> 
+                          <a href="https://www.linkedin.com/in/giorgio-rindonone-516b961b0/"><i class="fab fa-linkedin"></i></a>
+                      </span>
+                  </li>
+                  <li><span>Gianluigi Spisto </span>
+                      <span>
+                          <a href="https://github.com/glSpisto-92"><i class="fab fa-github"></i></a> 
+                          <a href="https://www.linkedin.com/in/gianluigi-spisto-8ab951190/"><i class="fab fa-linkedin"></i></a>
+                      </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-sm-6 col-xs-12">
+                <p class="copyright-text">Copyright &copy; 2021 All Rights Reserved by 
+             <a href="#">Booleat</a>.
+                </p>
+              </div>
+      
+            
+            </div>
+          </div>
+      
+
     </div>
 </footer>
--- routes/api.php
+++ routes/api.php
@@ -42,4 +42,3 @@ Route::namespace('Api')->group(function () {
 
 });
 
-
